35cc5c37bdce206b5adc93ce57a669847cb5eba6
Manu Mathew <mathmanu@users.noreply.github.com>
2017-06-27 20:20:50 +0530

    [incremental sparsity]  in progress

--- scripts/train_cifar10_classification.sh
+++ scripts/train_cifar10_classification.sh
@@ -29,6 +29,8 @@ stride_list="[1,1,2,1,2]"
 solver_param="{'type':'$type','base_lr':$base_lr,'max_iter':$max_iter,'test_interval':1000}"
 
 #-------------------------------------------------------
+weights='/user/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-models/pretrained/models/jacintonet11_cifar10_2017-06-23_15-23-30/initial/jacintonet11_cifar10_iter_64000.caffemodel'
+
 stage="initial"
 config_name=$folder_name/$stage;mkdir $config_name
 config_param="{'config_name':'$config_name','model_name':'$model_name','dataset':'$dataset','gpus':'$gpus',\
@@ -40,34 +42,23 @@ config_param="{'config_name':'$config_name','model_name':'$model_name','dataset'
 python ./models/image_classification.py --config_param="$config_param" --solver_param="$solver_param"
 config_name_prev=$config_name
 
-
-#Threshold step
-stage="threshold"
-weights=$config_name_prev/"$model_name"_"$dataset"_iter_$max_iter.caffemodel
-config_name="$folder_name"/$stage; echo $config_name; mkdir $config_name
-config_param="{'config_name':'$config_name','model_name':'$model_name','dataset':'$dataset','gpus':'$gpus',\
-'stride_list':$stride_list:'pretrain_model':'$weights',\
-'num_output':10,'image_width':32,'image_height':32,'crop_size':32,\
-'accum_batch_size':$batch_size,'batch_size':$batch_size,\
-'train_data':'./data/cifar10_train_lmdb','test_data':'./data/cifar10_test_lmdb',\
-'num_test_image':10000,'test_batch_size':50}" 
-$caffe threshold --threshold_fraction_low 0.40 --threshold_fraction_mid 0.80 --threshold_fraction_high 0.80 --threshold_value_max 0.2 --threshold_value_maxratio 0.2 --threshold_step_factor $threshold_step_factor --model="$config_name_prev/deploy.prototxt" --gpu="0" --weights=$weights --output=$config_name/"$model_name"_"$dataset"_iter_$max_iter.caffemodel
-config_name_prev=$config_name
-
-
 #-------------------------------------------------------
 #fine tuning
 stage="sparse"
 weights=$config_name_prev/"$model_name"_"$dataset"_iter_$max_iter.caffemodel
 
+max_iter=64000
 stepvalue1=32000
-stepvalue2=48000
+stepvalue2=56000
 type=SGD
 base_lr=0.01
 
 sparse_solver_param="{'type':'$type','base_lr':$base_lr,'max_iter':$max_iter,'test_interval':1000,\
 'lr_policy':'multistep','stepvalue':[$stepvalue1,$stepvalue2],\
-'sparse_mode':1,'display_sparsity':1000}"
+'sparse_mode':1,'display_sparsity':1000,\
+'sparsity_target':0.8,'sparsity_start_iter':10000,'sparsity_start_factor':0.50,\
+'sparsity_step_iter':1000,'sparsity_step_factor':0.01}"
+
 
 config_name="$folder_name"/$stage; echo $config_name; mkdir $config_name
 config_param="{'config_name':'$config_name','model_name':'$model_name','dataset':'$dataset','gpus':'$gpus',\
