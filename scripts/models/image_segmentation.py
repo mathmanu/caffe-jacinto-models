e4eae2de4515c98310165e13521c877ea2163e13
Manu Mathew <mathmanu@users.noreply.github.com>
2017-06-30 00:26:39 +0530

    [scripts] 4 threads seem to speedup

--- scripts/models/image_segmentation.py
+++ scripts/models/image_segmentation.py
@@ -208,28 +208,29 @@ def main():
         #get the proto string for the data layer in train phase seperately and return it
           
         train_proto_str = []
+        threads = 4
         if phase=='train' and config_param.use_image_list:                 
           data_kwargs = {'name': 'data', 'ntop':2, 
              'image_label_data_param': { 'image_list_path': config_param.train_data, 'label_list_path': config_param.train_label, 
-             'batch_size': config_param.train_batch_size_in_proto, 'scale_prob': 0.5, 'scale_min': 0.75, 'scale_max': 1.25, 'threads':1 } }      
+             'batch_size': config_param.train_batch_size_in_proto, 'scale_prob': 0.5, 'scale_min': 0.75, 'scale_max': 1.25, 'threads':threads } }      
           net['data'], net['label'] = L.ImageLabelListData(transform_param=config_param.train_transform_param, **data_kwargs)
           out_layer = 'data' 
         elif phase=='train':                 
           data_kwargs = {'name': 'data', 'ntop':2, 
              'image_label_data_param': { 'image_list_path': config_param.train_data, 'label_list_path': config_param.train_label,
-             'batch_size': config_param.train_batch_size_in_proto, 'backend':caffe_pb2.ImageLabelDataParameter.DB.Value('LMDB'), 'threads':1 } }      
+             'batch_size': config_param.train_batch_size_in_proto, 'backend':caffe_pb2.ImageLabelDataParameter.DB.Value('LMDB'), 'threads':threads } }      
           net['data'], net['label'] = L.ImageLabelData(transform_param=config_param.train_transform_param, **data_kwargs)
           out_layer = 'data'           
         elif phase=='test' and config_param.use_image_list:
           data_kwargs = { 'name': 'data', 'ntop':2, 
              'image_label_data_param': { 'image_list_path': config_param.test_data, 'label_list_path': config_param.test_label, 
-              'batch_size': config_param.test_batch_size_in_proto, 'scale_prob': 0.5, 'scale_min': 0.75, 'scale_max': 1.25, 'threads':1 } }         
+              'batch_size': config_param.test_batch_size_in_proto, 'scale_prob': 0.5, 'scale_min': 0.75, 'scale_max': 1.25, 'threads':threads } }         
           net['data'], net['label'] = L.ImageLabelListData(transform_param=config_param.test_transform_param,**data_kwargs)
           out_layer = 'data'
         elif phase=='test':
           data_kwargs = { 'name': 'data', 'ntop':2, 
              'image_label_data_param': { 'image_list_path': config_param.test_data, 'label_list_path': config_param.test_label,
-              'batch_size': config_param.test_batch_size_in_proto, 'backend':caffe_pb2.ImageLabelDataParameter.DB.Value('LMDB'), 'threads':1 } }         
+              'batch_size': config_param.test_batch_size_in_proto, 'backend':caffe_pb2.ImageLabelDataParameter.DB.Value('LMDB'), 'threads':threads } }         
           net['data'], net['label'] = L.ImageLabelData(transform_param=config_param.test_transform_param,**data_kwargs)
           out_layer = 'data'          
         elif phase=='deploy':
