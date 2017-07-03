07caf5bfc6864a018d7b1dc6d5ced5fb5bf9f766
Manu Mathew <mathmanu@users.noreply.github.com>
2017-07-03 14:03:14 +0530

    [infer] incer segmentation is now working

--- scripts/infer_cityscapes_segmentation.sh
+++ scripts/infer_cityscapes_segmentation.sh
@@ -6,7 +6,7 @@ echo Logging output to "$LOG"
 
 #------------------------------------------------------
 #palette used to translate id's to colors - for 5 classes
-#palette5="[[0,0,0],[128,64,128],[220,20,60],[250,170,30],[0,0,142],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]"
+palette5="[[0,0,0],[128,64,128],[220,20,60],[250,170,30],[0,0,142],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]"
 
 #for 19 or 20 classes training of cityscapes, first convert to original labelIds and then apply the pallete
 #label_dict_20_to_34="{0:7, 1:8, 2:11, 3:12, 4:13, 5:17, 6:19, 7:20, 8:21, 9:22, 10:23, 11:24, 12:25, 13:26, 14:27, 15:28, 16:31, 17:32, 18:33, 19:0}"
@@ -16,12 +16,12 @@ echo Logging output to "$LOG"
 
 num_images=1000 #10 #1000
 crop=0 #"1024 512"
-resize=0 #"1024 512"
+resize="1024 512"
 
 
 #------------------------------------------------------
-model="trained/image_segmentation/cityscapes20_jsegnet21v2/initial/deploy.prototxt"
-weights="trained/image_segmentation/cityscapes20_jsegnet21v2/initial/cityscapes20_jsegnet21v2_iter_32000.caffemodel"
+model="../trained/image_segmentation/cityscapes5_jsegnet21v2/initial/deploy.prototxt"
+weights="../trained/image_segmentation/cityscapes5_jsegnet21v2/initial/cityscapes5_jsegnet21v2_iter_32000.caffemodel"
 
 #Infer
 #input="input/sample"
@@ -31,10 +31,11 @@ input="./data/val-image-list.txt"
 output="output/cityscapes"
 
 #Generate output images for chroma blended visualization
-#./tools/utils/infer_segmentation.py --crop $crop --resize $resize --model $model --weights $weights --input $input --output $output --num_images $num_images --resize_back --label_dict="$label_dict_20_to_34" --blend --palette="$palette34"
+./tools/utils/infer_segmentation.py --crop $crop --resize $resize --model $model --weights $weights --input $input --output $output --num_images $num_images --resize_back --blend --palette="$palette5"
+#--palette="$palette34" --label_dict="$label_dict_20_to_34"
 
-#Generate output images running the IOU measurement (using the measure_...  script)
-./tools/utils/infer_segmentation.py --crop $crop --resize $resize --model $model --weights $weights --input $input --output $output --num_images $num_images --resize_back 
-#--label_dict="$label_dict_20_to_34"
+
+#Generate output images for running the IOU measurement (using the measure_...  script)
+#./tools/utils/infer_segmentation.py --crop $crop --resize $resize --model $model --weights $weights --input $input --output $output --num_images $num_images --resize_back --label_dict="$label_dict_20_to_34"
 
 
