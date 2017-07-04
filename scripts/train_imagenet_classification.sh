41c064c8b7b875fd316ef7f123837a2fd84de587
Manu Mathew <mathmanu@users.noreply.github.com>
2017-07-04 09:59:22 +0530

    [scripts,models] update from caffe-0.15

--- scripts/train_imagenet_classification.sh
+++ scripts/train_imagenet_classification.sh
@@ -19,19 +19,16 @@ caffe="../../caffe-jacinto/build/tools/caffe.bin"
 
 #-------------------------------------------------------
 gpus="0,1,2"
-max_iter=100 #320000
-base_lr=0 #0.1
-#threshold_step_factor=1e-6
+max_iter=320000
+base_lr=0.1
 type=SGD
 solver_param="{'type':'SGD','base_lr':$base_lr,'max_iter':$max_iter}"
 
 #-------------------------------------------------------
-weights='/data/mmcodec_video2_tier3/users/manu/experiments/object/classification/2017.06.new_script/caffe-0.15/jacintonet11_imagenet_2017.06.12_lmdb_caffe-0.15-2gpu(60.89%)/stage0/jacintonet11_iter_320000.caffemodel'
-
 stage="initial"
 config_name=$folder_name/$stage;mkdir $config_name
 config_param="{'config_name':'$config_name','model_name':'$model_name','dataset':'$dataset','gpus':'$gpus',\
-'pretrain_model':'$weights'}" 
+'pretrain_model':None}" 
 python ./models/image_classification.py --config_param="$config_param" --solver_param="$solver_param"
 config_name_prev=$config_name
 
@@ -45,9 +42,10 @@ max_iter=320000
 type=SGD
 base_lr=0.01  #use a lower lr for fine tuning
 sparse_solver_param="{'type':'$type','base_lr':$base_lr,'max_iter':$max_iter,\
+'regularization_type':'L1','weight_decay':1e-5,\
 'sparse_mode':1,'display_sparsity':1000,\
-'sparsity_target':0.8,'sparsity_start_iter':0,'sparsity_start_factor':0.0,\
-'sparsity_step_iter':2000,'sparsity_step_factor':0.01}"
+'sparsity_target':0.8,'sparsity_start_iter':40000,'sparsity_start_factor':0.0,\
+'sparsity_step_iter':1000,'sparsity_step_factor':0.01}"
 
 config_name="$folder_name"/$stage; echo $config_name; mkdir $config_name
 config_param="{'config_name':'$config_name','model_name':'$model_name','dataset':'$dataset','gpus':'$gpus',\
@@ -62,6 +60,7 @@ stage="test"
 weights=$config_name_prev/"$dataset"_"$model_name"_iter_$max_iter.caffemodel
 
 test_solver_param="{'type':'$type','base_lr':$base_lr,'max_iter':$max_iter,\
+'regularization_type':'L1','weight_decay':1e-5,\
 'sparse_mode':1,'display_sparsity':1000}"
 
 config_name="$folder_name"/$stage; echo $config_name; mkdir $config_name
--- trained/image_classification/cifar10_jacintonet11v2/initial/train.sh
+++ trained/image_classification/cifar10_jacintonet11v2/initial/train.sh
@@ -1,4 +1,4 @@
 cd /home/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-models/scripts
 ../../caffe-jacinto/build/tools/caffe.bin train \
---solver="training/cifar10_jacintonet11v2_2017-06-28_18-56-45/initial/solver.prototxt" \
---gpu "0,1" 2>&1 | tee training/cifar10_jacintonet11v2_2017-06-28_18-56-45/initial/train.log
+--solver="training/cifar10_jacintonet11v2_2017-07-04_07-19-29/initial/solver.prototxt" \
+--gpu "0,1,2" 2>&1 | tee training/cifar10_jacintonet11v2_2017-07-04_07-19-29/initial/train.log
