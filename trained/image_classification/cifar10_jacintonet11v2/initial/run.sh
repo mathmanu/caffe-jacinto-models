dee168c0dd0dee201ec95f5a670f525047c4667e
Manu Mathew <mathmanu@users.noreply.github.com>
2017-08-01 15:20:22 +0530

    [cifar10_classification]  new trained cifar10 model

---
+++ trained/image_classification/cifar10_jacintonet11v2/initial/run.sh
@@ -0,0 +1,4 @@
+cd /home/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-models/scripts
+../../caffe-jacinto/build/tools/caffe.bin train \
+--solver="training/cifar10_jacintonet11v2_2017-08-01_13-11-28/initial/solver.prototxt" \
+--gpu "0,1,2" 2>&1 | tee training/cifar10_jacintonet11v2_2017-08-01_13-11-28/initial/run.log
--- trained/image_classification/cifar10_jacintonet11v2/test/test.sh
+++
@@ -1,6 +0,0 @@
-cd /home/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-models/scripts
-../../caffe-jacinto/build/tools/caffe.bin test \
---model="training/cifar10_jacintonet11v2_2017-07-04_07-19-29/test/test.prototxt" \
---iterations="200" \
---weights="training/cifar10_jacintonet11v2_2017-07-04_07-19-29/sparse/cifar10_jacintonet11v2_iter_64000.caffemodel" \
---gpu "0,1,2" 2>&1 | tee training/cifar10_jacintonet11v2_2017-07-04_07-19-29/test/test.log
