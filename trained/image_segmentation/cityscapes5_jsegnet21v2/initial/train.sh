1e05851b56589007db31a6705eeeb7f470d9ab2b
Manu Mathew <mathmanu@users.noreply.github.com>
2017-08-01 10:39:41 +0530

    [segmentation]  new trained cityscapes5_jsegnet21v2 sparse model

--- trained/image_segmentation/cityscapes5_jsegnet21v2/initial/train.sh
+++ trained/image_segmentation/cityscapes5_jsegnet21v2/initial/train.sh
@@ -1,5 +1,5 @@
 cd /home/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-models/scripts
 ../../caffe-jacinto/build/tools/caffe.bin train \
---solver="training/cityscapes5_jsegnet21v2_2017-07-02_23-02-42/initial/solver.prototxt" \
---weights="training/imagenet_jacintonet11_v2_bn_iter_160000.caffemodel" \
---gpu "0,1" 2>&1 | tee training/cityscapes5_jsegnet21v2_2017-07-02_23-02-42/initial/train.log
+--solver="training/cityscapes5_jsegnet21v2_2017-07-31_18-11-04/initial/solver.prototxt" \
+--weights="training/imagenet_jacintonet11v2_iter_320000.caffemodel" \
+--gpu "0,1,2" 2>&1 | tee training/cityscapes5_jsegnet21v2_2017-07-31_18-11-04/initial/train.log
