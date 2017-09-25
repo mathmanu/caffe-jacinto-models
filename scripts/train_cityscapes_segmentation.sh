0c98711db252a98fea4852e901fc71fb014db378
Manu Mathew <mathmanu@users.noreply.github.com>
2017-09-25 10:17:06 +0530

    segmentation - new training script - improved quality sparsification with latest fixes in caffe--jacinto (sparsity_threshold_maxratio = 0.1)

--- scripts/train_cityscapes_segmentation.sh
+++ scripts/train_cityscapes_segmentation.sh
@@ -81,7 +81,7 @@ weights=$config_name_prev/"$dataset"_"$model_name"_iter_$max_iter.caffemodel
 sparse_solver_param="{'type':'$type','base_lr':$base_lr,'max_iter':$max_iter,'lr_policy':'multistep','stepvalue':[$stepvalue1,$stepvalue2],\
 'regularization_type':'L1','weight_decay':1e-5,\
 'sparse_mode':1,'display_sparsity':1000,\
-'sparsity_target':0.8,'sparsity_start_iter':3000,'sparsity_start_factor':0.80,\
+'sparsity_target':0.8,'sparsity_start_iter':1000,'sparsity_start_factor':0.60,\
 'sparsity_step_iter':1000,'sparsity_step_factor':0.01}"
 
 config_name="$folder_name"/$stage; echo $config_name; mkdir $config_name
