e3757db36d2a5b346474bb465dc4364be9390b78
Manu Mathew <mathmanu@users.noreply.github.com>
2017-10-03 14:40:20 +0530

    trained models - correct sparsity measurement for the imagenet classification model

--- trained/image_classification/imagenet_jacintonet11v2/test/run.sh
+++ trained/image_classification/imagenet_jacintonet11v2/test/run.sh
@@ -2,5 +2,6 @@ cd /home/a0393608/files/work/code/vision/ti/bitbucket/algoref/caffe-jacinto-mode
 ../../caffe-jacinto/build/tools/caffe.bin test \
 --model="training/imagenet_jacintonet11v2_2017-08-14_19-50-34/test/test.prototxt" \
 --iterations="1000" \
+--display_sparsity=1 \
 --weights="training/imagenet_jacintonet11v2_2017-08-14_19-50-34/sparse/imagenet_jacintonet11v2_iter_160000.caffemodel" \
 --gpu "0,1,2" 2>&1 | tee training/imagenet_jacintonet11v2_2017-08-14_19-50-34/test/run.log
