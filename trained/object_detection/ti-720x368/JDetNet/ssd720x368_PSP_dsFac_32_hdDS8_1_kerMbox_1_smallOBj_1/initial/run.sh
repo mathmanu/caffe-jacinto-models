5d9ac7dc4af9e2d3174dc8deea76dd25eb6c7d1c
Soyeb Nagori <soyebn@users.noreply.github.com>
2018-04-01 14:23:53 +0530

    [REL_SSD]removing other than VOC0712 models

--- trained/object_detection/ti-720x368/JDetNet/ssd720x368_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1/initial/run.sh
+++
@@ -1,4 +0,0 @@
-/user/a0875091/files/work/bitbucket_TI/caffe-jacinto//build/tools/caffe.bin train \
---solver="training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/initial/solver.prototxt" \
---snapshot="/user/a0875091/files/work/bitbucket_TI/caffe-jacinto-models/scripts/training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/initial/ti-vgg-720x368-v2_ssdJacintoNetV2_iter_18000.solverstate" \
---gpu "0,1" 2>&1 | tee -a training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/initial/run.log
--- trained/object_detection/ti-720x368/JDetNet/ssd720x368_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1/l1reg/run.sh
+++
@@ -1,4 +0,0 @@
-/user/a0875091/files/work/bitbucket_TI/caffe-jacinto//build/tools/caffe.bin train \
---solver="training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/l1reg/solver.prototxt" \
---weights="training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/initial/ti-vgg-720x368-v2_ssdJacintoNetV2_iter_10000_55.92.caffemodel" \
---gpu "0,1" 2>&1 | tee training/ti-vgg-720x368-v2/JDetNet/ssd768x320_PSP_dsFac_32_hdDS8_1_kerMbox_1_smallOBj_1_51.41/l1reg/run.log
